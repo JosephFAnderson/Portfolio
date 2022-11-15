13bea234e98b9c8625f1ab872ca70e6869d6363f
Joseph A <joseph_anderson11@yahoo.com>
2022-11-15 18:01:14 -0500

    set up

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import './app.css'
+
+function App() {
+  return (
+    <div style={{margin: 0, height: "100vh", backgroundColor: "#264d59	", color: "white"}}>
+      {/* <Navbar /> */}
+      {/* <Footer /> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import githubLogo from './githubLogo.png'
+import './footer.css'
+
+function Footer() {
+
+    return (
+        <footer>
+          <a href="https://github.com/JosephFAnderson"><img src={githubLogo} href=""/></a>
+        </footer>
+    )
+}
+
+export default Footer;
