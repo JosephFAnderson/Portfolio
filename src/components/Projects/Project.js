7b96abcb8a387d7cc17071b6a77de206dbbf315f
Joseph A <joseph_anderson11@yahoo.com>
2022-11-23 10:01:50 -0500

    project cards

--- src/components/Projects/Project.js
+++ src/components/Projects/Project.js
@@ -1,8 +1,26 @@
+import projects from '../../static/projects';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import './Project.css';
+
 function Project() {
+  console.log(projects);
     return (
-        <>
-          <p>Projects</p>
-        </>
+        <div id="projects-container">
+          {projects.map(project => (
+            <>
+              <div className="project-card" key={project.name}>
+                <img className="project-image" src={project.image} draggable="false"></img>
+                <div className="project-text">
+                  <p>{project.name}</p>
+                  <p>{project.description}</p>
+                  <p>Technologies {project.technologies}</p>
+                  <a className="project-github" href={project.github}><FontAwesomeIcon icon={faGithub} /></a>
+                </div>
+              </div>              
+            </>
+          ))}
+        </div>
     )
 }
 
---
+++ src/static/projects.js
@@ -0,0 +1,28 @@
+const projects = [
+    {
+        name: "RPG the Game",
+        description: "A combat based game that allows users to defeat monsters for xp and gold. Level up and upgrade your gear to battle more challenging monsters.",
+        technologies: "Handlebars, JavaScript, MySQL, Express.js",
+        url: "https://rpgthegame.herokuapp.com/",
+        github: "https://github.com/JosephFAnderson/RPG_the_Game",
+        image: "./rpgthegame.png"
+    },
+    {
+        name: "Tech Blog",
+        description: "A blog post forum for users to create post about a topic, that other users can comment on.",
+        technologies: "Handlesbars, JavaScript, MySQL, Express.js",
+        url: "https://jatechblog.herokuapp.com/",
+        github: "https://github.com/JosephFAnderson/Tech-Blog",
+        image: "./techblog.png"
+    },
+    {
+        name: "PokeGameCenter",
+        description: "One stop place for pokemon information. Search up a pokemon to be presented with pokedex, tcg, and vgc information",
+        technologies: "HTML, CSS, JavaScript",
+        url: "https://josephfanderson.github.io/PokeGameCenter/",
+        github: "https://github.com/JosephFAnderson/PokeGameCenter",
+        image: "./pokegamecenter.png"
+    }
+];
+
+export default projects
